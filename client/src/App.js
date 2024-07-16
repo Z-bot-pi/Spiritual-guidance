2f3ca51932172b918de993d75a84258baf58708d
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-16 07:02:23 +0000

    client

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Button } from 'antd';
 import 'antd/lib/button/style/css';
 function App() {
   return (
-    <div className="App">
+    <div className="App p-5">
       <h1>SPIRITUAL GUIDANCE</h1>
       <Button type="primary">Primary Button</Button>
     </div>
