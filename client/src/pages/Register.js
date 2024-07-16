298ffadd432a3b0ada069c4744844ceb50bcec5b
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-16 15:12:25 +0000

    client

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,19 @@
+import { Input } from 'antd';
 import React from 'react';
+import { Form } from 'react-router-dom';
 
 function Register() {
     return (
     <div className='authentication'>
-         <div className='authentication-form'>
-            <h1>Register</h1> 
+         <div className='authentication-form card p-2'>
+            <h1 className='card-title'>Pleasure To Meet You</h1>
+            <Form>
+                
+                     <Form.Item label='Name' name='name'>
+                         <Input placeholder='Name'/>           
+                     </Form.Item>
+
+             </Form>
                 </div>
                 
          </div>
