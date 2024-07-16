8cb82441f94c2c4420f41098a5deefdd68a7ffd7
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-16 02:36:59 +0000

    importing button

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import 'antd/dist/antd.css';
+import (Button) from 'antd'
 function App() {
   return (
     <div className="App">
       <h1>SPIRITUAL GUIDANCE</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
