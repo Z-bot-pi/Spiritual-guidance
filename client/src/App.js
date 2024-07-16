09ddc5e4986c871f9bc839afdf5362649e5397a8
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-16 07:36:48 +0000

    client

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ function App() {
     
     <Routes> 
 
-       <Route path='/login' element={Login}/>
-       <Route path='/register' element={Register}/>
+       <Route path='/login' element={<Login />}/>
+       <Route path='/register' element={<Register />}/>
     </Routes>
 
     </BrowserRouter>
