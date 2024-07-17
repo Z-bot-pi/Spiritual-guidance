b8b1504cae6b71da281b5d8d9e0036338ea83227
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-17 03:57:54 +0000

    user

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const userRoute = require('./routes/userRoute');
+
+app.use('/api/users', userRoute);
 const port = process.env.PORT || 5000;
 
 
