74ad89230e975a3a046cb3bb264e24fffb927b1c
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-17 04:18:22 +0000

    packages

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,16 @@
-const mongoose = require("mangoose");
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("MongoDB is connected"))
+  .catch((error) => console.log("Error in MongoDB connection", error));
 
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
-    console.log("MongoBD is connected");
+    console.log("MongoDB is connected");
 });
 
 connection.on("error", (error) => {
