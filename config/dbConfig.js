bfcdc74d4948e430823cea6f8911575fa9af3e77
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-17 13:14:18 +0000

    client

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,21 +2,37 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
-.then(() => console.log("MongoDB is connected"))
-.catch((error) => console.error("Error connecting to MongoDB:", error));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // Remove useCreateIndex and use createIndexes in the URI
+  // useCreateIndex: true,
+  // Instead, include createIndexes in the URI options
+  useFindAndModify: false,
+  dbName: 'spiritualguidance-alx', // Replace dbName with your database name
+  retryWrites: true,
+  w: 'majority'
+});
 
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
-    console.log("MongoDB connected");
+  console.log("MongoDB connected");
 });
 
 connection.on("error", (error) => {
-    console.error("MongoDB connection error:", error);
+  console.error("MongoDB connection error:", error);
 });
 
 module.exports = mongoose;
+
+
+
+
+
+
+
+
+
+
+
+
