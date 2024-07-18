3e3c3f781e50a65d29ed805a417a21ef6d778572
Zintle Mazibuko <140543586+Z-bot-pi@users.noreply.github.com>
2024-07-18 21:28:31 +0000

    client

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
-import LandingPage from './pages/LandingPage'; // Import the LandingPage component
+import LandingPage from './pages/LandingPage';
+import PrivateRoute from './components/PrivateRoute'; // Import the PrivateRoute component
 import { Toaster } from 'react-hot-toast';
 
 function App() {
@@ -13,11 +14,20 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/landing' element={<LandingPage />} /> {/* Add this route */}
+        <Route path='/landing' element={<LandingPage />} />
+        <Route
+          path='/home'
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/' element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
