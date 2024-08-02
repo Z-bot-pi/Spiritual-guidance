3a3b3044c2c08c8f5c414a85fc7e030ba007fd49
Z-bot-pi <xulubanazintle@gmail.com>
2024-08-02 11:13:50 +0000

    appointment

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Appointment = require('../models/AppointmentModel');
+const Appointment = require('../models/appointmentModel');
 const jwt = require('jsonwebtoken');
 
 const defaultGuider = 'Default Spiritual Guider';
