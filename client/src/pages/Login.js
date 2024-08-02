dba68cfc623ea4220f6be0e315a680ecf94b89eb
Z-bot-pi <xulubanazintle@gmail.com>
2024-08-02 09:31:03 +0000

    Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import './Login.css'; // Import the CSS file
 
 const Login = () => {
   const [email, setEmail] = useState('');
